2652845e00a42996764e5839ac4dd1ba0aad69f2
ksalayon <switchashanti@gmail.com>
2025-04-01 10:57:20 +1300

    Fix login issues - inner join with roles table to be able to return role key
    Omit password from user response

--- controllers/api/loginController.js
+++ controllers/api/loginController.js
@@ -6,16 +6,24 @@ const {createTokens} = require("../tokensController");
 async function login(req, res) {
     const {email, password} = req.body;
     try {
-        const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        const { rows } = await pool.query('SELECT users.id, users.name, users.email, ' +
+            'users.created, users.role_id, users.password,' +
+            'roles.name as role' +
+            ' FROM users ' +
+            ' LEFT JOIN  roles ' +
+            ' ON users.role_id = roles.id ' +
+            'WHERE users.email = $1', [email]);
         const user = rows[0];
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return getResponseStatusWithMessage(res, 401, responseModel.GENERAL);
         }
 
         const {shortLived, longLived} = await createTokens(user.id);
-        res.cookie('llt', longLived, { httpOnly: true, secure: true, sameSite: 'Strict' })
-        res.status(200).json({slt: shortLived, user});
+        res.cookie('llt', longLived, { httpOnly: true, secure: true, sameSite: 'Strict' });
+        const { password, ...safeUser } = user;
+        res.status(200).json({slt: shortLived, user: safeUser});
     } catch (error)  {
+        console.log('login error', error);
         sendServerError(res, responseModel.USER);
     }
 
